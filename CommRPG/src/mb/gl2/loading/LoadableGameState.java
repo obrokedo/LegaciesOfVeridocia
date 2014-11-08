51c677c2a25084b02863fb62c6b2f337959dda6f
obrokedo <obrokedo@a067d575-7f83-4c51-aa8f-3b14720980f8>
2014-11-08 06:24:01 +0000

    - SVN crushing my soul 3

--- CommRPG/src/mb/gl2/loading/LoadableGameState.java
+++
@@ -1,8 +0,0 @@
-package mb.gl2.loading;
-
-import org.newdawn.slick.state.BasicGameState;
-
-public abstract class LoadableGameState extends BasicGameState 
-{
-	public abstract void stateLoaded(ResourceManager resourceManager);
-}
--- CommRPG/src/mb/gl2/loading/ResourceManagerListener.java
+++
@@ -1,5 +0,0 @@
-package mb.gl2.loading;
-
-public interface ResourceManagerListener {
-	public void resourceManagerChanged(ResourceManager resourceManager);
-}
