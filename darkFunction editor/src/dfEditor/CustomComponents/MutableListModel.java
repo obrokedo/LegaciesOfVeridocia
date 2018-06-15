8cf3e8bb0c33428993f154ba0eddb33518e6bc9f
obrokedo <obrokedo@a067d575-7f83-4c51-aa8f-3b14720980f8>
2018-06-15 15:15:05 +0000

    Deleting

--- darkFunction editor/src/dfEditor/CustomComponents/MutableListModel.java
+++
@@ -1,9 +0,0 @@
-package dfEditor.CustomComponents;
-
-import javax.swing.*;
-
-// @author Santhosh Kumar T - santhosh@in.fiorano.com 
-public interface MutableListModel extends ListModel { 
-    public boolean isCellEditable(int index); 
-    public void setValueAt(Object value, int index); 
-} 
